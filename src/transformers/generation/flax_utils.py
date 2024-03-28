86ceebeb2eb3ff4d8c1e33212e1d4e5fd8f553d0
Joy Lunkad <joylunkad@gmail.com>
2024-03-28 15:07:22 +0530

    Update flax_utils.py

--- src/transformers/generation/flax_utils.py
+++ src/transformers/generation/flax_utils.py
@@ -63,6 +63,7 @@ class FlaxGreedySearchOutput(ModelOutput):
     """
 
     sequences: jnp.ndarray = None
+    logits: jnp.ndarray = None
 
 
 @flax.struct.dataclass
@@ -100,6 +101,7 @@ class FlaxBeamSearchOutput(ModelOutput):
 class GreedyState:
     cur_len: jnp.ndarray
     sequences: jnp.ndarray
+    logits: jnp.ndarray
     running_token: jnp.ndarray
     is_sent_finished: jnp.ndarray
     model_kwargs: Dict[str, jnp.ndarray]
@@ -574,7 +576,9 @@ class FlaxGenerationMixin:
         max_length = max_length if max_length is not None else self.generation_config.max_length
         pad_token_id = pad_token_id if pad_token_id is not None else self.generation_config.pad_token_id
         eos_token_id = eos_token_id if eos_token_id is not None else self.generation_config.eos_token_id
-
+        # vocab_size = self.config.get("vocab_size", self.generation_config.vocab_size)
+        vocab_size = self.config.vocab_size
+        
         batch_size, cur_len = input_ids.shape
 
         eos_token_id = jnp.array(eos_token_id, dtype=jnp.int32 if eos_token_id is not None else None)
@@ -584,6 +588,9 @@ class FlaxGenerationMixin:
         # per batch-item holding current token in loop.
         sequences = jnp.full((batch_size, max_length), pad_token_id, dtype=jnp.int32)
         sequences = lax.dynamic_update_slice(sequences, input_ids, (0, 0))
+        
+        # per batch-item holding current logits in loop.
+        logits = jnp.zeros((batch_size, max_length, vocab_size), dtype=jnp.float32)
 
         # per batch-item state bit indicating if sentence has finished.
         is_sent_finished = jnp.zeros((batch_size,), dtype=jnp.bool_)
@@ -598,6 +605,7 @@ class FlaxGenerationMixin:
         state = GreedyState(
             cur_len=cur_len,
             sequences=sequences,
+            logits=logits,
             running_token=input_ids,
             is_sent_finished=is_sent_finished,
             model_kwargs=model_kwargs,
@@ -613,8 +621,10 @@ class FlaxGenerationMixin:
         def greedy_search_body_fn(state):
             """state update fn."""
             model_outputs = model(state.running_token, params=params, **state.model_kwargs)
+            state_logits = lax.dynamic_update_slice(state.logits, model_outputs.logits, (0, 0, 0)) 
+            
             logits = model_outputs.logits[:, -1]
-
+            
             # apply min_length, ...
             logits = logits_processor(state.sequences, logits, state.cur_len)
 
@@ -626,9 +636,11 @@ class FlaxGenerationMixin:
 
             next_sequences = lax.dynamic_update_slice(state.sequences, next_token, (0, state.cur_len))
             next_model_kwargs = self.update_inputs_for_generation(model_outputs, state.model_kwargs)
+            
             return GreedyState(
                 cur_len=state.cur_len + 1,
                 sequences=next_sequences,
+                logits=state_logits,
                 running_token=next_token,
                 is_sent_finished=next_is_sent_finished,
                 model_kwargs=next_model_kwargs,
@@ -643,7 +655,7 @@ class FlaxGenerationMixin:
         else:
             state = lax.while_loop(greedy_search_cond_fn, greedy_search_body_fn, state)
 
-        return FlaxGreedySearchOutput(sequences=state.sequences)
+        return FlaxGreedySearchOutput(sequences=state.sequences, logits=state.logits)
 
     def _sample(
         self,
