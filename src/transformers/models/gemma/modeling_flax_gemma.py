d116b3cb2beedded79a88045935ae3524fddcf37
Joy Lunkad <joylunkad@gmail.com>
2024-03-28 16:46:25 +0530

    Update modeling_flax_gemma.py

--- src/transformers/models/gemma/modeling_flax_gemma.py
+++ src/transformers/models/gemma/modeling_flax_gemma.py
@@ -717,14 +717,22 @@ class FlaxGemmaForCausalLMModule(nn.Module):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
-
+        
+        jax.debug.print(
+            "FlaxGemmaForCausalLMModule.__call__ : self.model.apply - {}", outputs
+        )
+        
         hidden_states = outputs[0]
         if self.config.tie_word_embeddings:
             shared_kernel = self.model.variables["params"]["embed_tokens"]["embedding"].T
             lm_logits = self.lm_head.apply({"params": {"kernel": shared_kernel}}, hidden_states)
         else:
             lm_logits = self.lm_head(hidden_states)
-
+        
+        jax.debug.print(
+            "FlaxGemmaForCausalLMModule.__call__ : lm_logits - {}", lm_logits
+        )
+        
         if not return_dict:
             return (lm_logits,) + outputs[1:]
 
